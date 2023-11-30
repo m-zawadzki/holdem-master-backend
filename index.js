c8733bb6419f0c8f0046faafb2f4f072fa999f65
m-zawadzki <zwodek01@gmail.com>
2023-11-30 19:13:51 +0100

    Implemented simple connection with fe

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import { PokerGame } from './pokerGame.js';
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: process.env.FRONTEND_URL || 3000,
-    },
+  cors: {
+    origin: process.env.FRONTEND_URL || 3000,
+  },
 });
 
 const PORT = process.env.PORT || 4000;
@@ -16,61 +17,82 @@ const PORT = process.env.PORT || 4000;
 const rooms = new Map();
 
 const emitPlayersUpdated = (roomId, players) => {
-    io.to(roomId).emit('playersUpdated', { players });
-}
+  io.to(roomId).emit('playersUpdated', { players });
+};
+
+const emitStartGame = (socket, roomId, players) => {
+  io.to(roomId).emit('gameStarted');
+
+  const pokerGame = new PokerGame(players, socket, io);
+  pokerGame.playGame();
+};
+
+const joinRoom = socket => {
+  socket.on('joinRoom', roomId => {
+    socket.join(roomId);
+
+    console.log(`User joined room: ${roomId}`);
+
+    if (!rooms.has(roomId)) {
+      rooms.set(roomId, { players: [] });
+    }
+
+    rooms.get(roomId).players.push(socket.id);
+
+    const playersCount = rooms.get(roomId).players.length;
+
+    emitPlayersUpdated(roomId, playersCount);
+
+    if (playersCount >= 2) {
+      emitStartGame(socket, roomId, rooms.get(roomId).players);
+    }
+  });
+};
+
+const leaveRoom = socket => {
+  socket.on('leaveRoom', roomId => {
+    socket.leave(roomId);
+    console.log(`User left room: ${roomId}`);
+
+    if (rooms.has(roomId)) {
+      const room = rooms.get(roomId);
+      room.players = room.players.filter(playerId => playerId !== socket.id);
+      rooms.set(roomId, room);
+
+      emitPlayersUpdated(roomId, room.players.length);
+    }
+  });
+};
+
+const disconnect = socket => {
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+
+    if (rooms.size > 0) {
+      rooms.forEach((room, roomId) => {
+        if (room.players.includes(socket.id)) {
+          room.players = room.players.filter(
+            playerId => playerId !== socket.id
+          );
+          rooms.set(roomId, room);
+
+          emitPlayersUpdated(roomId, room.players.length);
+        }
+      });
+    }
+  });
+};
 
 io.on('connection', socket => {
-    console.log('User connected');
+  console.log('User connected');
 
-    socket.on('joinRoom', roomId => {
-        socket.join(roomId);
-        console.log(`User joined room: ${roomId}`);
+  joinRoom(socket);
 
-        if (!rooms.has(roomId)) {
-            rooms.set(roomId, { players: [] });
-        }
-        rooms.get(roomId).players.push(socket.id);
+  leaveRoom(socket);
 
-        const playersCount = rooms.get(roomId).players.length;
-        emitPlayersUpdated(roomId, playersCount)
-    });
-
-    socket.on('leaveRoom', roomId => {
-        socket.leave(roomId);
-        console.log(`User left room: ${roomId}`);
-
-        if (rooms.has(roomId)) {
-            const room = rooms.get(roomId);
-            room.players = room.players.filter(playerId => playerId !== socket.id);
-            rooms.set(roomId, room);
-
-            emitPlayersUpdated(roomId, room.players.length)
-        }
-    });
-
-    socket.on('pokerAction', data => {
-        const { roomId } = data;
-        io.to(roomId).emit('pokerUpdate', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-
-        if (rooms.size > 0) {
-            rooms.forEach((room, roomId) => {
-                if (room.players.includes(socket.id)) {
-                    room.players = room.players.filter(
-                        playerId => playerId !== socket.id
-                    );
-                    rooms.set(roomId, room);
-
-                    emitPlayersUpdated(roomId, room.players.length)
-                }
-            });
-        }
-    });
+  disconnect(socket);
 });
 
 server.listen(PORT, () => {
-    console.log(`Listening at http://localhost:${PORT}`);
+  console.log(`Listening at http://localhost:${PORT}`);
 });
---
+++ pokerGame.js
@@ -0,0 +1,135 @@
+class Card {
+  constructor(suit, rank) {
+    this.suit = suit;
+    this.rank = rank;
+  }
+}
+
+class Deck {
+  constructor() {
+    this.suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+    this.ranks = [
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      'Jack',
+      'Queen',
+      'King',
+      'Ace',
+    ];
+    this.cards = this.initializeDeck();
+    this.shuffleDeck();
+  }
+
+  initializeDeck() {
+    const deck = [];
+    for (const suit of this.suits) {
+      for (const rank of this.ranks) {
+        deck.push(new Card(suit, rank));
+      }
+    }
+    return deck;
+  }
+
+  shuffleDeck() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+
+  dealCards(numberOfPlayers, cardsPerHand) {
+    const players = new Array(numberOfPlayers).fill([]).map(() => []);
+    for (let i = 0; i < cardsPerHand; i++) {
+      for (let j = 0; j < numberOfPlayers; j++) {
+        const card = this.cards.pop();
+        if (card) {
+          players[j].push(card);
+        }
+      }
+    }
+    return players;
+  }
+}
+
+export class PokerGame {
+  constructor(players, socket, io) {
+    this.deck = new Deck();
+    this.players = players;
+    this.cardsPerHand = 2;
+    this.communityCards = [];
+    this.playerHands = [];
+    this.currentRound = 0;
+    this.currentBet = 0;
+    this.playerBets = new Array(this.players.length).fill(0);
+    this.socket = socket;
+    this.io = io;
+  }
+
+  startRound() {
+    console.log(`Round ${this.currentRound + 1} started.`);
+    this.playerHands = this.deck.dealCards(
+      this.players.length,
+      this.cardsPerHand
+    );
+    this.playerHands.forEach((cards, index) => {
+      this.io.to(this.players[index]).emit('dealingResult', cards);
+    });
+    this.currentRound++;
+  }
+
+  startBettingRound() {
+    console.log('Betting Round started.');
+    this.playerBets = new Array(this.players.length).fill(10);
+  }
+
+  dealFlop() {
+    console.log('Dealing Flop.');
+    this.communityCards = this.communityCards.concat(
+      this.deck.dealCards(this.players.length, 3)[0]
+    );
+  }
+
+  dealTurn() {
+    console.log('Dealing Turn.');
+    this.communityCards = this.communityCards.concat(
+      this.deck.dealCards(this.players.length, 1)[0]
+    );
+  }
+
+  dealRiver() {
+    console.log('Dealing River.');
+    this.communityCards = this.communityCards.concat(
+      this.deck.dealCards(this.players.length, 1)[0]
+    );
+  }
+
+  startFinalBettingRound() {
+    console.log('Final Betting Round started.');
+    this.playerBets = new Array(this.players.length).fill(10);
+  }
+
+  showdown() {
+    console.log('Showdown!');
+    const winnerIndex = this.playerBets.indexOf(Math.max(...this.playerBets));
+    console.log(`Player ${winnerIndex + 1} wins the game!`);
+  }
+
+  playGame() {
+    this.startRound();
+    this.startBettingRound();
+    this.dealFlop();
+    this.startBettingRound();
+    this.dealTurn();
+    this.startBettingRound();
+    this.dealRiver();
+    this.startFinalBettingRound();
+    this.showdown();
+  }
+}
